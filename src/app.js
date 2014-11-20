2dd4bc4975fa2afe28f166fa80c99165e07acaa1
Alexandr Evseev <evseev@aboutecho.com>
2014-11-20 18:40:58 +0400

    Change 'no items' message style

--- src/app.js
+++ src/app.js
@@ -360,8 +360,8 @@ sources.methods.handlers.onError = function(data, options) {
 };
 
 sources.css =
-	'.{class:empty} { border: 1px solid #d2d2d2; margin: 0 5px 10px 5px; padding: 30px 0; text-align: left; }' +
-	'.{class:empty} .{class:message} { background: url("//cdn.echoenabled.com/apps/echo/conversations/v2/sdk-derived/images/info.png") no-repeat; margin: 0 auto; font-size: 14px; font-family: "Helvetica Neue", Helvetica, "Open Sans", sans-serif; padding-left: 40px; display: block; width: 180px; line-height: 16px; color: #7f7f7f; }' +
+	'.{class:empty} { border: 1px solid #d2d2d2; background-color: #fff; margin: 0 5px 10px 5px; padding: 30px 0; text-align: center; }' +
+	'.{class:empty} .{class:message} { text-align: left; background: url("//cdn.echoenabled.com/apps/echo/conversations/v2/sdk-derived/images/info.png") no-repeat; margin: 0 auto; font-size: 14px; font-family: "Helvetica Neue", Helvetica, "Open Sans", sans-serif; padding-left: 40px; display: inline-block; line-height: 16px; color: #7f7f7f; }' +
 	'.{class:container} { margin: 0px auto; }' +
 	'.{class:graph} { width: 100%; }';
 
